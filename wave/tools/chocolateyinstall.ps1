81195da7f8f448545869ffe872d57d9268449df2
Evan Simkowitz <esimkowitz@users.noreply.github.com>
2024-10-22 20:33:24 -0700

    setup package

---
+++ wave/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64        = 'https://dl.waveterm.dev/releases-w2/Wave-win32-x64-0.8.12.exe'
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url64bit      = $url64
+
+  softwareName  = 'Wave' 
+  checksum64    = 'A0749E49A492F3C2B4D36701A3BEE346D40FB4ECDF611DB6FF4F15CD798A6778F7C863CFED4C8DAA5F4D3CCC63F9EFAB89A9279E46B3966A2069F6BBD52F1115'
+  checksumType64= 'sha512'
+  silentArgs   = '/S'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs # https://docs.chocolatey.org/en-us/create/functions/install-chocolateypackage
---
+++ wave/update.ps1
@@ -0,0 +1,40 @@
+# Lots of learnings for this script come care of Maurice Kevenaar
+# https://github.com/mkevenaar
+
+import-module Chocolatey-AU
+
+function global:au_BeforeUpdate($Package) {}
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"           #1
+            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"      #2
+        }
+        ".\wave.nuspec" = @{
+            "(<version>)[^<]*" = "$($Latest.Version)"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $Releases = "https://api.github.com/repos/wavetermdev/waveterm/releases/latest"
+    $PageSource = Invoke-RestMethod -Uri $Releases
+
+    if ($PageSource.tag_name -match 'v([\d.-]+)') {
+        $Version = $Matches[1]
+    } else {
+        throw "Unable to grab tag name"
+    }
+
+    Write-Host "newversion=$($Version)"
+
+    Write-Output "newversion=$($Version)" >> $Env:GITHUB_OUTPUT
+
+    @{
+        Version = $Version
+        Url64 = "https://dl.waveterm.dev/releases-w2/Wave-win32-x64-$($Version).exe"
+    }
+}
+
+update -ChecksumFor none
