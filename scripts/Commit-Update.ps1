13fcf8a3675d7ad6fcb5ab50b41fa2ed91edb6b1
Evan Simkowitz <esimkowitz@users.noreply.github.com>
2024-11-21 19:48:05 -0800

    Validated script on Windows

---
+++ scripts/Commit-Update.ps1
@@ -0,0 +1,14 @@
+$HEAD = $(git rev-parse HEAD)
+$NUSPEC_B64 = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes([System.IO.File]::ReadAllText('wave\wave.nuspec')))
+$CHOCOLATEY_B64 = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes([System.IO.File]::ReadAllText('wave\tools\chocolateyinstall.ps1')))
+$QUERY = @"
+mutation createCommit {
+  createCommitOnBranch(input: {branch: {repositoryNameWithOwner: "wavetermdev/chocolatey", branchName: "main"}, message: {headline: "Update hello.txt"}, fileChanges: {additions: [{path: "wave/wave.nuspec", contents: "$NUSPEC_B64"},{path: "wave/tools/chocolateyinstall.ps1", contents:"$CHOCOLATEY_B64"}]}, expectedHeadOid: "$HEAD"}) {
+    commit {
+      url
+    }
+  }
+}
+"@
+
+gh api graphql -f query=$QUERY  
