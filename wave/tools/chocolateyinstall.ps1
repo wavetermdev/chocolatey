d45906a7f812888e7674e5f2fe08de8679c4e466
Evan Simkowitz <esimkowitz@users.noreply.github.com>
2024-11-21 19:47:18 -0800

    Update hello.txt

--- wave/tools/chocolateyinstall.ps1
+++ wave/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
-﻿$ErrorActionPreference = 'Stop'
+$ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64        = 'https://dl.waveterm.dev/releases-w2/Wave-win32-x64-0.8.12.msi'
+$url64        = 'https://dl.waveterm.dev/releases-w2/Wave-win32-x64-0.9.3.msi'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
@@ -8,7 +8,7 @@ $packageArgs = @{
   url64bit      = $url64
 
   softwareName  = 'Wave' 
-  checksum64    = '064194834A092496B9BB3CED219C683F47A05E68A5E3F180C56C33A4FBB6CA6F02945E5812756E4B6004BF139946D2D07B0674DBEC883D2351AFEFF991680374'
+  checksum64    = '7b2802a8a2b86bae9dd973114435883cdb5dea4986c2804892be997e64c3d35b058a054adde57be5bacfe544583f35cff84df24f70e3c9e5b2b03d880d376b5c'
   checksumType64= 'sha512'
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
